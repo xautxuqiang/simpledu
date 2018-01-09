103c3fddc2c09b886ee33adfad742522f84e879b
xautxuqiang <xautxuqiang@126.com>
2018-01-09 19:08:16 +0800

    first commit

---
+++ simpledu/config.py
@@ -0,0 +1,18 @@
+class BaseConfig(object):
+    SECRET_KEY = 'makesure to set a very secret key'
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:xqxq1994@localhost:3306/simpledu?charset=utf8'
+
+class ProductionConfig(BaseConfig):
+    pass
+
+class TestingConfig(BaseConfig):
+    pass
+
+configs = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'testing': TestingConfig
+}
---
+++ simpledu/models.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), unique=True, index=True, nullable=False)
+    publish_courses = db.relationship('Course')
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class Course(db.Model):
+    __tablename__ = 'courses'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), unique=True, index=True, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
+    author = db.relationship('User', uselist=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    def __repr__(self):
+        return '{}'.format(self.name)
